1baa5169c1abe2f13c9031114504883dcdfbff80
Denislav883 <yanev.denislav@gmail.com>
2025-09-19 22:14:19 +0300

    show home page

--- src/index.js
+++ src/index.js
@@ -1,1 +1,26 @@
-console.log("hello world!");
+import http from "http";
+import homeHtml from "./home.html.js";
+import siteCss from "./site.css.js";
+
+const server = http.createServer((req, res) => {
+
+    if(req.url === "/") {
+        res.writeHead(200, {
+            "content-type": "text.html"
+        });
+
+            res.write(homeHtml);
+    } else if(req.url === "/styles/site.css") {
+        res.writeHead(200, {
+            "content-type": "text/css"
+        })
+
+        res.write(siteCss)
+    }
+
+    res.end();
+})
+
+server.listen(5000);
+
+console.log("Server is listening on http://localhost:5000...");
