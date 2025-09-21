b53d10e27c02f933e3dcbe04b87102915e82e2ef
Denislav883 <yanev.denislav@gmail.com>
2025-09-21 11:36:08 +0300

    create add breed page

--- src/index.js
+++ src/index.js
@@ -1,30 +1,51 @@
 import http from "http";
 import fs from "fs/promises"
 
-import siteCss from "./site.css.js";
-
 const server = http.createServer(async (req, res) => {
+    let html;
 
-    if(req.url === "/") {
-        const homeHtml = await fs.readFile("./src/views/home/index.html", {encoding: "utf-8"});
+    switch (req.url) {
+        case "/":
+            html = await homeView();
+            break;
 
-        res.writeHead(200, {
-            "content-type": "text.html"
-        });
+        case "/cats/add-breed":
+            html = await addBreedView();
+            break;
 
-            res.write(homeHtml);
-    } else if(req.url === "/styles/site.css") {
-        const siteCss = await fs.readFile("./src/styles/site.css", {encoding: "utf-8"});
+        case "/styles/site.css":
+            const siteCss = await fs.readFile("./src/styles/site.css", {encoding: "utf-8"});
 
-        res.writeHead(200, {
-            "content-type": "text/css"
-        })
+            res.writeHead(200, {
+                "content-type": "text/css"
+            })
 
-        res.write(siteCss)
+            res.write(siteCss);
+            res.end();
+            return;
+        default:
+            return res.end();
     }
 
+    res.writeHead(200, {
+        "content-type": "text.html"
+    });
+
+    res.write(html);
     res.end();
 })
+
+async function homeView() {
+    const html = await fs.readFile("./src/views/home/index.html", {encoding: "utf-8"});
+
+    return html;
+}
+
+async function addBreedView() {
+    const html = await fs.readFile("./src/views/addBreed.html", {encoding: "utf-8"});
+
+    return html;
+}
 
 server.listen(5000);
 
