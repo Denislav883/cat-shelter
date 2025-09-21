fad39625ae404c1e5b07989d9d9a1500f163bcc0
Denislav883 <yanev.denislav@gmail.com>
2025-09-21 19:45:18 +0300

    add cat

---
+++ src/cats.js
@@ -0,0 +1,30 @@
+export default [
+   {
+    id: 1,
+    imageUrl: "https://ichef.bbci.co.uk/news/976/cpsprodpb/12A9B/production/_111434467_gettyimages-1143489763.jpg",
+    name: "Pretty Kitty",
+    breed: "Bombay Cat",
+    description: "Dominant and aggressive to other cats. Will probably eat you in your sleep. Very cute tho."
+   },
+   {
+    id: 2,
+    imageUrl: "https://cdn.pixabay.com/photo/2015/06/19/14/20/cat-814952_1280.jpg",
+    name: "Navcho",
+    breed: "Persian Cat",
+    description: "A talkative and affectionate cat with striking yellow eyes."
+   },
+   {
+    id: 3,
+    imageUrl: "https://cdn.pixabay.com/photo/2018/08/08/05/12/cat-3591348_1280.jpg",
+    name: "Sisa",
+    breed: "Siamase Cat",
+    description: "Loves to cuddle and nap. Requires regular grooming for its luxurious coat."
+   },
+   {
+    id: 4,
+    imageUrl: "https://cdn.pixabay.com/photo/2017/02/20/18/03/cat-2083492_1280.jpg",
+    name: "Garry",
+    breed: "Bombay Cat",
+    description: "Mysterious and elegant. Often found lounging in sunny spots."
+   },
+];
--- src/index.js
+++ src/index.js
@@ -1,8 +1,26 @@
 import http from "http";
 import fs from "fs/promises"
 
+import cats from "./cats.js"
+
 const server = http.createServer(async (req, res) => {
     let html;
+
+    if(req.method === "POST") {
+        let data = "";
+
+        req.on("data", (chunk) => {
+            data += chunk.toString();
+        });
+
+        req.on("end", () => {
+            const searchParams = new URLSearchParams(data);
+
+            const newCat = Object.fromEntries(searchParams.entries());
+
+            cats.push(newCat);
+        });
+    }
 
     switch (req.url) {
         case "/":
@@ -46,7 +64,11 @@ function readFile(path) {
 async function homeView() {
     const html = await readFile("./src/views/home/index.html");
 
-    return html;
+    const catsHtml = cats.map(cat => catTemplate(cat)).join("\n");
+
+    const result = html.replace("{{cats}}", catsHtml);
+
+    return result;
 }
 
 async function addBreedView() {
@@ -59,6 +81,21 @@ async function addCatView() {
     const html = await readFile("./src/views/addCat.html");
 
     return html;
+}
+
+function catTemplate(cat) {
+    return `
+        <li>
+            <img src="${cat.imageUrl}" alt="${cat.name}">
+            <h3></h3>
+            <p><span>Breed: </span>${cat.breed}</p>
+            <p><span>Description: </span>${cat.description}</p>
+            <ul class="buttons">
+                <li class="btn edit"><a href="">Change Info</a></li>
+                <li class="btn delete"><a href="">New Home</a></li>
+            </ul>
+        </li>
+    `
 }
 
 server.listen(5000);
